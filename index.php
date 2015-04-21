1e966d1513203528ce267ff91d0963fed1e518d2
manojkumaran <micman.manoj@gmail.com>
2015-04-21 23:39:24 +0530

    about us section content added

--- index.php
+++ index.php
@@ -9,20 +9,28 @@
 <!--\ BLOCKQUOTE  END /-->
 
 <!--\ ABOUT GREENISH HEALTHCARE BEGIN /-->
+<section class="container">
+<h2> About Greenish Healthcare</h2>
+<article class="col-sm-12 col-md-10 col-lg-10 about-us">
 <p>
-Greenish India is focusing towards promoting healthier and safer phytomedicines ( plant based medecine with no chemicals added) since herbal renaissance is blooming across the world. 
+Greenish India is focusing towards promoting healthier and safer phytomedicines ( plant based medicine with no chemicals added) since herbal renaissance is blooming across the world. 
 Herbal medicine acts as either “preventive” or as “Nutritive”. Hence we are always delighted to offer a variety of products with superior quality that are made from high quality Black Seeds.
 We are committed to provide a Supreme range of Black seed herbal products that comes along the human life for their well being. 
 Black seed is a Prophetic medicine to strengthen the immune system and various organs of the human body. 
 It’s a wonder herb that has been used for centuries throughout the world for its many therapheutic potentials.
-Our objective is to provide you with a product that supports and promotes natural healing through an eco friendly way.
+Our objective is to provide you with a product that supports and promotes natural healing through an eco-friendly way.
 Our products are developed under the able guidance of leading phytochemists and Pharmacognosists, 
 so as to make our products internationally acceptable and recognizable and to provide an evidence based relief to the ailing society.
 The present products developed are based on black cumin seeds, a prophetic medicines to contribute significantly to the healthcare of human society.
 Our products are free from adulterants and substitutes. Our products are prepared employing good laboratory and manufacturing products.
 We humbly take pride in declaring that we were the leading exporter of Black Cumin Seed Oil from India. 
-( courtesy :  Infodrive India , Source :  Statistics driven from  Dept of Customs, Govt. of India )
+( courtesy :  Infodrive India , Source :  Statistics driven from  Dept. of Customs, Govt. of India.)
 </p>
+</article>
+<article class="hidden-xs col-md-2 col-lg-2">
+	<img src="img/about-img.png" alt="greenish healthcare"/>
+</article>
+</section>
 <!--\ ABOUT GREENISH HEALTHCARE END /-->
 
 <!--\ TESTIMONIALS BEGIN /-->
