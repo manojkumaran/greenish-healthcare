8b12323656740d41616e0bb6e4fb2f0d53f5cf14
Arunkumar S <sail.arunkumar@gmail.com>
2015-04-25 13:48:54 +0530

    Style changes and script changes for Testimonial

--- js/main.js
+++ js/main.js
@@ -8,3 +8,42 @@ $('.bxslider').bxSlider({
 });
 
 
+/** TESTIMONIAL **/
+
+Element.prototype.readMore = function ( config ) {
+	this.className += ' read-more-excerpt read-more';
+	this.style.height = config.height + 'px';
+	var readMoreText = document.createElement('span');
+	readMoreText.innerHTML = 'Read More';
+	readMoreText.className = 'read-more-link';
+	this.parentNode.insertBefore(readMoreText, this.nextSibling);
+};
+
+Element.prototype.showMore = function () {
+	var prev = this.previousSibling;
+	prev.style.height = 'auto';
+	this.previousSibling.classList.remove('read-more');
+	this.remove();
+};
+
+document.getElementById('testimonial').addEventListener ( 'click', function ( event ) {
+	var tgt = event.target;
+	if ( tgt.className === 'read-more-link' ) {
+		tgt.showMore();
+	}
+});
+
+jQuery.fn.extend ({
+	readMore: function ( config ) {
+		jQuery(this).each(function () {
+			jQuery(this)[0].readMore( config );
+		});
+	}
+});
+jQuery('.testimonial-content').readMore({
+	'height': 30
+});
+
+/** TESTIMONIAL **/
+
+
