86e33c48e907b4e7ad6c0974b7ccda10be46f333
Sudhasankar <devivit@gmail.com>
2015-04-27 23:51:40 +0530

    Edited Carousel.php

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,13 +1,29 @@
 	<section class="container">
 		<h2> <span> Our Products</span></h2>
 	<div id="pdt-carousel">
-			<div class="item"><img src="./img/pr1.png" alt="product name"></div>
-			<div class="item"><img src="./img/pr2.png" alt="product name"></div>
-			<div class="item"><img src="./img/pr3.png" alt="product name"></div>
-			<div class="item"><img src="./img/pr4.png" alt="product name"></div>
-			<div class="item"><img src="./img/pr1.png" alt="product name"></div>
-			<div class="item"><img src="./img/pr1.png" alt="product name"></div>
-			<div class="item"><img src="./img/pr1.png" alt="product name"></div>
-			<div class="item"><img src="./img/pr1.png" alt="product name"></div>			
+			<div class="item"><img src="./img/pr1.png" alt="product name">
+			<p> Greenish Black Seed </p>
+			</div>
+			<div class="item"><img src="./img/pr2.png" alt="product name">
+			<p> Greenish Black Seed </p>
+			</div>
+			<div class="item"><img src="./img/pr3.png" alt="product name">
+			<p> Greenish Black Seed </p>
+			</div>
+			<div class="item"><img src="./img/pr4.png" alt="product name">
+			<p> Greenish Black Seed </p>
+			</div>
+			<div class="item"><img src="./img/pr1.png" alt="product name">
+			<p> Greenish Black Seed </p>
+			</div>
+			<div class="item"><img src="./img/pr2.png" alt="product name">
+			<p> Greenish Black Seed </p>
+			</div>
+			<div class="item"><img src="./img/pr3.png" alt="product name">
+			<p> Greenish Black Seed </p>
+			</div>
+			<div class="item"><img src="./img/pr4.png" alt="product name">
+			<p> Greenish Black Seed </p>
+			</div>			
      </div>
 	</section>
