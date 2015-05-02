91873714ca1b7307a3641d8cdea8afa5c5ff5491
Sudhasankar <devivit@gmail.com>
2015-05-02 16:04:53 +0530

    Added plp.php

---
+++ plp.php
@@ -0,0 +1,60 @@
+<?php include("includes/header.php"); ?>
+
+<section class="container our_products">
+	<h2> Our Products</h2>
+</section>
+<section class="container">
+<article class="row clearfix">
+					<ul class="menus list-unstyled list-inline prod_links col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<li class="col-lg-4"><a href="#">Health Care</a></li>
+						<li class="col-lg-4"><a href="#">Skin &amp; Hair Care </a></li>
+						<li class="col-lg-4"><a href="#">Pain Care</a></li>
+					</ul>
+</article>
+</section>
+<section class="container">
+<article class="row clearfix product_img">
+<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+<a href=""><img src="img/pr1.png" alt=""><p>Greenish Black Seed</p></a>
+
+</div>
+<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+<a href=""><img src="img/pr2.png" alt=""><p>Greenish Black Seed</p></a>
+</div>
+<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+<a href=""><img src="img/pr3.png" alt=""><p>Greenish Black Seed</p></a>
+</div>
+<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+<a href=""><img src="img/pr4.png" alt=""><p>Greenish Black Seed</p></a>
+</div>
+
+</article>
+
+<article class="row clearfix product_img">
+<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+<a href=""><img src="img/pr1.png" alt=""><p>Greenish Black Seed</p></a>
+
+</div>
+<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+<a href=""><img src="img/pr2.png" alt=""><p>Greenish Black Seed</p></a>
+</div>
+<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+<a href=""><img src="img/pr3.png" alt=""><p>Greenish Black Seed</p></a>
+</div>
+<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+<a href=""><img src="img/pr4.png" alt=""><p>Greenish Black Seed</p></a>
+</div>
+
+</article>
+</section>
+
+
+
+
+
+
+
+
+
+
+<?php include("includes/footer.php"); ?>
