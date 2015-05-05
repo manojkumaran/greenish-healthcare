7f34d3144bed10156e03ad856d32cf775da02cf6
Sudhasankar <devivit@gmail.com>
2015-05-05 08:03:45 +0530

    pdp page commited..

---
+++ pdp.php
@@ -0,0 +1,65 @@
+<?php include("includes/header.php"); ?>
+<section class="container">
+	<article class="row clearfix">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<ol class="breadcrumb">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Library</a></li>
+			<li class="active">Data</li>
+		</ol>
+		</div>
+	</article>
+</section>
+<section class="container">
+	<article class="row clearfix">
+	<div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+	<img src="img/pr2.png" alt="" />
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 product_heading">
+	<h3>Greenish Safeway</h3>
+	<p> Herbal vital Capsule - 100% Natural </p>
+	<div class="product_details">
+	<label>PACKING</label>
+	<p>60 Capsules per bottle</p>
+	<label>DIRECTION FOR USE</label>
+	<p>One Capsule morning before breakfast</p>
+	<p>One Capsule night before dinner</p>
+	
+	</div>
+	</div>
+	</article>
+</section>
+<section class="container">
+	<article class="row clearfix">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</div>
+	</article>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("includes/footer.php"); ?>
