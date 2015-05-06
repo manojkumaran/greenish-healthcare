445a072698c737b6853c496bc8940de78d1b4a5f
manojkumaran <micman.manoj@gmail.com>
2015-05-07 00:31:12 +0530

    pdp for baraka added

---
+++ baraka.php
@@ -0,0 +1,160 @@
+<?php include("includes/header.php"); ?>
+<section class="container">
+	<article class="row clearfix">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<ol class="breadcrumb">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li class="active">Greenish Baraka - Black Seed Oil</li>
+		</ol>
+		</div>
+	</article>
+</section>
+<section class="container pdt-main">
+	<article class="row clearfix">
+	<div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+	<img src="img/greenish-baraka.jpg" alt="Greenish Baraka - Black Seed Oil" />
+	</div>
+	
+	<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 product_heading">
+		<h3>Greenish Baraka - Black Seed Oil</h3>
+		<p> Strengthen Your Immune Naturally </p>
+		<div class="product_details col-lg-6">
+		<label>BENEFITS</label>
+		<p>
+			<ul>
+				<li> Builds a strong and healthy immune system </li>
+				<li> Supports overall systemic balance and cellular health </li>
+				<li> Helps to protect the body against threats </li>
+				<li> Improves energy levels and vitality </li>
+				<li> Promotes healthy circulation and blood flow </li>
+				<li> Maintains healthy liver functioning </li>
+				<li> Safe and effective concentrated tincture </li>
+			</ul>
+		</p>
+	</div>
+	</div>
+	
+	</article>
+</section>
+<section class="container pdt-details-container">
+	<article class="row clearfix">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<p>
+				Black seed is one of the most admired herbs of all times through out the world for its extraordinary utility towards the healing of many diseases including incurable ones. Having understood the above, The Greenish India offers pure extract of Black seeds , without damaging its therapeutic potentials , in the form of Greenish Baraka which mainly focuses and boosts up the Human immune power.
+				With the latest techniques and advanced extraction processes, we extract the maximum amount of nutrients from the Black Seeds with out adding any preservatives, which result in the consumer receiving Black Seed oil of the purest quality. Greenish Baraka Black seed Oil can be used both internally as well as externally.
+				In order to offer the product with the highest standard, every bottle of Greenish Baraka undergoes several quality control measures, which meets an ultimate excellence.
+				Greenish Baraka is available in a hygienic 50ml and 100ml bottles respectively.
+			</p>
+			
+			<div>
+				<h4 class="underline">Uses and Remedies of Greenish Baraka Black seed oil:</h4>
+				<p>
+					<span class="boldfs">General Health and Immune booster:</span> Greenish Baraka activates the body immune system and increases resistance to diseases. Take half a teaspoon (2.5ml) of Greenish Baraka oil in a glass of warm water/(preferably jeera water) daily in the morning and evening with empty stomach. It enhance the immune power of the body against any diseases.
+				</p>
+				
+				<ol>
+					<li>
+						<p>
+							<span class="boldfs">Cough and Asthma:</span>
+							Apply Greenish Baraka oil on the chest and the back. Mix half a teaspoon (2.5ml) of Greenish Baraka oil in boiling water /(preferably jeera water), inhale the vapor and exhale facing upwards. Repeat the process till the vapor exhausts. The remaining water can be taken inside in the morning and evening.
+						</p>
+					</li>
+					
+					<li>
+						<p>
+							<span class="boldfs">Diabetes:</span>
+							Take half a teaspoon (2.5ml) of Greenish Baraka oil in ½ a glass of lukewarm water / (preferably jeera water) in the morning with the empty stomach as well as before dinner. Avoid intake of any foods for 1 hour after the above mentioned medication.
+						</p>
+					</li>
+
+					<li>
+						<p>
+							<span class="boldfs">Hypertension / Blood pressure:</span>
+							With the empty stomach, take half a teaspoon (2.5ml) of Greenish Baraka oil after chewing a lobe of garlic. Apart from this, apply Greenish Baraka oil on the whole body and expose it to the sun twice a week.
+						</p>
+					</li>
+
+					<li>
+						<p>
+							<span class="boldfs">Eye-sight improvement and vitamin deficiency:</span>
+							 Greenish Baraka acts as an effective vitamin supplement against eye-sight deficiency. Mix half a teaspoon (2.5ml)of Greenish Baraka oil with carrot juice and drink twice a day.
+						</p>
+					</li>
+
+					<li>
+						<p>
+							<span class="boldfs">Gynaecological:</span>
+							During the menstrual period take half a teaspoon of Greenish Baraka oil with warm milk during the bedtime. Continued intake will cleans the uterus and regulates the periods.
+						</p>
+					</li>
+
+					<li>
+						<p>
+							<span class="boldfs">Ulcer:</span>
+							Take half a teaspoon (2.5ml) of Greenish Baraka oil with 1 glass of white pumpkin juice in the morning and evening with empty stomach. Avoid intake of any foods for 1 hour after the above mentioned medication.
+						</p>
+					</li>
+
+					<li>
+						<p>
+							<span class="boldfs">For cure from skin diseases / to get a lustrous skin:</span>
+							Mix equal quantity of Greenish Baraka oil and pure coconut oil and apply it on the affected area of the skin. For preservation of luster and glow of the skin, mix equal quantity of Greenish Baraka oil and olive oil and apply it on the skin. Same mixture can also be used to develop a soft and healthy skin for children.
+						</p>
+					</li>		
+
+					<li>
+						<p>
+							<span class="boldfs">Loss of hair and premature graying:</span>
+							Apply lemon juice (or) paste made of gooseberry on the scalp and take bathe after 15 minutes. After thoroughly drying the hair, apply Greenish Baraka oil on the scalp. In addition, take half a teaspoon (2.5ml) of Greenish Baraka with carrot juice every morning so as to bring the hair falling to an end.
+						</p>
+					</li>		
+
+					<li>
+						<p>
+							<span class="boldfs">Toothache:</span>
+							Mix half a (2.5ml) teaspoon of Greenish Baraka with warm water and gargle the mouth. Apply Greenish Baraka on the affected tooth.
+						</p>
+					</li>	
+					
+					<li>
+						<p>
+							<span class="boldfs">Muscular pain / Rheumatism:</span>
+							Mildly heat a required quantity of Greenish Baraka oil and apply on the affected part by massaging the bone. Take half a teaspoon (2.5ml) of Greenish Baraka oil with a glass of warm water in the morning as well as in the evening.
+						</p>
+					</li>						
+				</ol>
+				<p>** It is not advisable to take Greenish Baraka during pregnancy without consulting a physician</p>
+			</div>
+		
+		</div>
+
+	</article>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("includes/footer.php"); ?>
