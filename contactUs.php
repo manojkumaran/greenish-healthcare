cefd08d685e43402d9ff166f5e662b67bd2554eb
Sudhasankar <devivit@gmail.com>
2015-05-20 00:04:01 +0530

    Included contactus.php

---
+++ contactUs.php
@@ -0,0 +1,66 @@
+<?php include("includes/header.php"); ?>
+
+
+<section class="container" >
+<article class="row clearfix contact-us">
+<h2> Contact Us </h2>
+		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 form-footer clearfix">
+			<h3 class="footer_contact">Send Your Requirements</h3>
+			<form>
+				<input type="text" class="form-control" placeholder="NAME" />
+				<input type="text" class="form-control"placeholder="CONTACT NUMBER"/>
+				<input type="text" class="form-control"placeholder="E-MAIL" />
+				<textarea class="form-control"placeholder="COMMENTS" ></textarea>
+				<input type="submit" value="SEND" class="action-btn-footer btn btn-primary"/> 
+			</form>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 address-block1 clearfix">
+			<h3 class="footer_contact footer-head">For More Informations, Contact</h3>
+			
+			<div class="col-lg-5 clearfix contact-details">
+			<img src="img/about-img.png" class="inline-block"/><h3 class="address-heading">Greenish HealthCare</h3>
+				<p>Greenish (India) Trades Pvt. Ltd.</p>
+			<p >New no 35 Old no 15, </p>
+			<p>Santhome High Road,</p>
+			<p>	Mylapore, </p>
+			<p>Chennai-600004</p>
+			</div>
+				<div class="row col-lg-5 contacts contact-details1">
+				<p>		Mobile: +(91)-960010199.9941111000</p>
+				<p>	Phone: +91-44-24672095</p>
+				<p>	Fax: +91-44-24670669</p>
+				<p>	Customer Care: 9600101999</p>
+				<p>	Email: greenishindiacare@gmail.com</p>
+				
+			</div>
+		</div>
+
+</article>
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("includes/footer.php"); ?>
